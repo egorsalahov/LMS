ad7954a01420c2365aa28666b0c5283437bc9a88
EgorSalahov <egorsalahov2@gmail.com>
2026-04-04 12:45:36 +0300

    first test

---
+++ EgorSalahovSemestrovka22/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EgorSalahovSemestrovka22.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
