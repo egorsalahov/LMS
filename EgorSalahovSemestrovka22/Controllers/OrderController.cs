b76b200b072e6d910ffa3acd7182f49bebc56c3f
EgorSalahov <egorsalahov2@gmail.com>
2026-05-14 15:29:43 +0300

    OrderController (верстка)

---
+++ EgorSalahovSemestrovka22/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EgorSalahovSemestrovka22.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
