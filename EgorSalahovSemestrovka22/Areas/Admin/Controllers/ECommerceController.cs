cc1c0840a47c3937334d35bbbd6844e1a689ee0b
EgorSalahov <egorsalahov2@gmail.com>
2026-05-17 15:03:55 +0300

    Admin ECommerce Page

---
+++ EgorSalahovSemestrovka22/Areas/Admin/Controllers/ECommerceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EgorSalahovSemestrovka22.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ECommerceController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult Customers()
+        {
+            return View();
+        }
+        public IActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
